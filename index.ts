2015d5beba90fe7d8c3f4e45c58eb9da66dba049
Theo Ojochegbe <symplytheo@gmail.com>
2023-01-27 10:18:11 +0100

    feat: initialize with express + typescript

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Response, Request } from 'express';
+
+const app: Express = express();
+
+const PORT: number = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Nannynest API');
+});
+
+app.listen(PORT, () => {
+  console.log(`[SERVER]: Listening at localhost:${PORT}`);
+});
