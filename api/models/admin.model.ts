3a88b628fb2d9abbb8d92391ade0422acb56100a
Theo Ojochegbe <symplytheo@gmail.com>
2023-03-16 19:46:52 +0100

    fix: hide password on returned object

--- api/models/admin.model.ts
+++ api/models/admin.model.ts
@@ -26,6 +26,7 @@ adminSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.password;
   },
 });
 
